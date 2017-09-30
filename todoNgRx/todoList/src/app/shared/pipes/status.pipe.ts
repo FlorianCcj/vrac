20cdc517e6993ac51ccc59bc188ad19706bbe980
FlorianCcj <palmade.florian@gmail.com>
2017-09-30 14:26:18 +0200

    [x] [reducer/pipe] debug reducer path, add pipe s code

--- todoNgRx/todoList/src/app/shared/pipes/status.pipe.ts
+++ todoNgRx/todoList/src/app/shared/pipes/status.pipe.ts
@@ -1,12 +1,15 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import {PipeTransform, Pipe} from "@angular/core";
+import {IN_PROGRESS, DONE} from "../../core/store/filters/filters.actions";
 
-@Pipe({
-  name: 'status'
-})
-export class StatusPipe implements PipeTransform {
+@Pipe({name: 'status'})
+export class StatusPipe implements PipeTransform{
 
-  transform(value: any, args?: any): any {
-    return null;
-  }
+    transform(values:any, status: string):any {
+        if (status === IN_PROGRESS)
+            return values.filter((todo) => !todo.done);
+        else if (status === DONE)
+            return values.filter((todo) => todo.done);
+        return values;
+    }
 
 }
