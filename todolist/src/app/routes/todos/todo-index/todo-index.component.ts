f809ef3b734560983d5c07c6be8937edfea603c8
FlorianCcj <palmade.florian@gmail.com>
2017-10-15 19:06:10 +0200

    [+] [platform] add platform exemple

--- todolist/src/app/routes/todos/todo-index/todo-index.component.ts
+++ todolist/src/app/routes/todos/todo-index/todo-index.component.ts
@@ -2,8 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { Store } from "@ngrx/store";
 import { Observable } from "rxjs/Observable";
 import * as TodosActions from "../../../core/store/todos/todos.actions";
+import * as FilterVisibilityAction from "../../../core/store/todos/visibility-filter.actions";
 import { TodosEffects } from "../../../core/store/todos/todos.effects";
 import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/take';
 
 @Component({
   selector: 'app-todo-index',
@@ -15,6 +17,9 @@ export class TodoIndexComponent implements OnInit {
   todos : Observable<any>;
   addTodoSuccess$ : Observable<any>;
 
+  filters = [{id: "SHOW_ALL", title: "All"}, {id: "SHOW_COMPLETED", title: "Completed"}, { id: "SHOW_ACTIVE", title: "Active" }];
+  activeFilter : Observable<any>;
+
   constructor(
     private store : Store<any>,
     private todosEffects: TodosEffects
@@ -22,6 +27,7 @@ export class TodoIndexComponent implements OnInit {
   	this.store.dispatch(new TodosActions.GetTodos());
     this.todos = store.select("todos");
     this.addTodoSuccess$ = this.todosEffects.addTodo$.filter(( {type} ) => type === TodosActions.ADD_TODO_SUCCESS);
+    this.activeFilter = store.select("visibilityFilter").take(1);
   }
 
   ngOnInit() {
@@ -29,6 +35,11 @@ export class TodoIndexComponent implements OnInit {
 
   addTodo( todo ) {
     this.store.dispatch(new TodosActions.AddTodo(todo));
+  }
+
+  changeFilter( filter ) {
+    this.store.dispatch(new FilterVisibilityAction.SetVisibilityFilter(filter));
+    this.store.dispatch(new TodosActions.GetTodos());
   }
 
 }
