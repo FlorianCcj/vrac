36524a4a764ed4cca30a886043b9648ca2d2af7c
FlorianCcj <palmade.florian@gmail.com>
2017-10-15 16:00:03 +0200

    [+] [type et class] ajout des types et des classes dans les actions

--- todolist/src/app/core/store/todos/todos.actions.ts
+++ todolist/src/app/core/store/todos/todos.actions.ts
@@ -1,6 +1,45 @@
 import { Injectable } from '@angular/core';
 import { Action } from '@ngrx/store';
 
+export const GET_TODOS = "GET_TODOS";
+export const GET_TODOS_SUCCESS = "GET_TODOS_SUCCESS";
+export const GET_TODOS_ERROR = "GET_TODOS_ERROR";
+
+export const ADD_TODO = "ADD_TODO";
+export const ADD_TODO_SUCCESS = "ADD_TODO_SUCCESS";
+export const ADD_TODO_ERROR = "ADD_TODO_ERROR";
+
+export class GetTodos implements Action {
+  readonly type = GET_TODOS;
+}
+export class GetTodosSuccess implements Action {
+  readonly type = GET_TODOS_SUCCESS;
+  constructor(public payload: any) {}
+}
+export class GetTodosError implements Action {
+  readonly type = GET_TODOS_ERROR;
+}
+
+export class AddTodo implements Action {
+  readonly type = ADD_TODO;
+  constructor(public payload: any) {}
+}
+export class AddTodoSuccess implements Action {
+  readonly type = ADD_TODO_SUCCESS;
+}
+export class AddTodoError implements Action {
+  readonly type = ADD_TODO_ERROR;
+}
+
+export type Actions =
+  | GetTodos
+  | GetTodosSuccess
+  | GetTodosError
+  | AddTodo
+  | AddTodoSuccess
+  | AddTodoError
+;
+
 @Injectable()
 export class TodosActions {
 
