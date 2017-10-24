a73f45e604f57311ca5b5c767a5cb57180b8e472
FlorianCcj <palmade.florian@gmail.com>
2017-10-24 18:33:16 +0200

    [-] [todolist] changement de repos

--- todoList/src/app/core/store/messenger/messenger.actions.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-import { Action } from '@ngrx/store';
-
-export const ADD_SYSTEM_S_INFOS = '[Messenger] Add System\'s infos';
-
-export class AddSystemsInfo implements Action {
-  readonly type = ADD_SYSTEM_S_INFOS;
-  constructor(public payload: any) {}
-}
--- todoList/src/app/core/store/todos/visibility-filter.actions.ts
+++
@@ -1,9 +0,0 @@
-import { Action } from '@ngrx/store';
-
-export const SET_VISIBILITY_FILTER = "SET_VISIBILITY_FILTER";
-
-export class SetVisibilityFilter implements Action {
-  readonly type = SET_VISIBILITY_FILTER;
-  payload: string;
-  constructor(public filter : string) { this.payload = filter; }
-}
